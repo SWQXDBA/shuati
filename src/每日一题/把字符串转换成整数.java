c136f004515e07239013deb02c469b34715a6df4
SWQXDBA <983110853@qq.com>
2021-04-17 21:28:56 +0800

    1

---
+++ src/每日一题/把字符串转换成整数.java
@@ -0,0 +1,33 @@
+package 每日一题;
+
+import java.util.Scanner;
+
+public class 把字符串转换成整数 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String str = scanner.nextLine();
+            char[] chars = str.toCharArray();
+            long num = 0;
+            long flag = 1;
+            for (int i = 0; i < chars.length; i++) {
+                if (chars[i] == '+')
+                    continue;
+                if (chars[i] == '-') {
+                    flag = -1;
+                    continue;
+                }
+
+                if (chars[i] < '0' || chars[i] > '9') {
+                    num = 0;
+                    break;
+                }
+                num += chars[i] - '0';
+                if (i != chars.length - 1)
+                    num *= 10;
+            }
+            num *= flag;
+            System.out.println(num);
+        }
+    }
+}
