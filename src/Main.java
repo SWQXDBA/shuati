84bcc9be796b7a27d54d79dff8f865a14a09fa7f
SWQXDBA2 <SWQXDBA2>
2021-05-01 18:38:42 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
- interface Main {
+import javax.swing.*;
+import java.awt.*;
 
+public class Main {
 
- }
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jFrame.setVisible(true);
+        jFrame.setSize(800, 20);
+        jFrame.setTitle("登录");
+        jFrame.setLocationRelativeTo(null);
+        JLabel label = new JLabel("账户");
+        label.setFont(new Font("宋体", Font.BOLD, 20));
+        jFrame.add(label);
+    }
+}
 
