01393d48be69949e1555957f2a044b875e998303
SWQXDBA2 <SWQXDBA2>
2021-06-13 22:29:36 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,6 @@
 class test {
     public static void main(String[] args) {
-        A<String> a = new A<>("aaa");
-        System.out.println(a.get());
-
-
-    }
-
-    static class A<T> {
-        T value;
-
-        A(T value) {
-            this.value = value;
-        }
-
-        T get() {
-            return value;
-        }
+        //摸鱼的一天
     }
 
 }
