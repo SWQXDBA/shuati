36824e2d5e0862eb9a9ee5a2dda131c23e2ab649
SWQXDBA <983110853@qq.com>
2021-07-21 15:22:41 +0800

    1

---
+++ src/力扣题目/二叉树/最近的公共祖先.java
@@ -0,0 +1,34 @@
+package 力扣题目.二叉树;
+
+import MyTools.TreeNode;
+
+public class 最近的公共祖先 {
+    //第一种 后序遍历
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        //先看左子树有没有公共祖先
+        TreeNode node = lowestCommonAncestor(root.left, p, q);
+        if (node != null) {
+            return node;
+        } else {
+            node = lowestCommonAncestor(root.right, p, q);
+        }
+        ////再看右子树有没有公共祖先
+        if (node != null) {
+            return node;
+        } else {
+            //看看自己是不是公共祖先
+            node = hasSubNode(root, p) && hasSubNode(root, q) ? root : null;
+        }
+        return node;
+
+    }
+
+    boolean hasSubNode(TreeNode root, TreeNode target) {
+        return root == null ? target == null : root == target || hasSubNode(root.left, target) || hasSubNode(root.right, target);
+    }
+
+
+}
