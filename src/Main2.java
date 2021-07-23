afc1cf6b9f576f1b8d2c2b2f6ec184464f4b3a26
SWQXDBA2 <SWQXDBA2>
2021-07-24 04:03:16 +0800

    1

--- src/Main2.java
+++ src/Main2.java
@@ -1,6 +1,12 @@
 public class Main2 {
-    public static void main(String[] args) {
 
-        // MySort.runTest(MySort.SortWay.millionsDataSorts, 10, 10000000);/
+    public static void main(String[] args) {
+        Object o = new Object() {
+            @Override
+            public boolean equals(Object obj) {
+                return true;
+            }
+        };
+        System.out.println(o.equals("aaa"));
     }
 }
