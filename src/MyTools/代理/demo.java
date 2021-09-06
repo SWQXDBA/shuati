c797e15f3c387603259808332ae9b90e0e25fcf0
SWQXDBA <983110853@qq.com>
2021-09-06 14:23:10 +0800

    1

---
+++ src/MyTools/代理/demo.java
@@ -0,0 +1,44 @@
+package MyTools.代理;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+public class demo {
+
+    public static void main(String[] args) {
+        new demo().test();
+
+    }
+
+    void test() {
+        //接口的实现类
+        Animal bird = () -> System.out.println("鸟在吃");
+
+        //处理器 在其中调用接口的方法 以及进行其他代理操作
+        //这里实现的是invoke方法
+        InvocationHandler handler = (proxy, method, argss) -> {
+            System.out.println("处理器1执行插入的代码");
+            Object result = method.invoke(bird, argss);
+            System.out.println("处理器1执行插入的代码2");
+            return result;
+        };
+        InvocationHandler handler2 = (proxy, method, argss) -> {
+            System.out.println("处理器2执行插入的代码");
+            Object result = method.invoke(bird, argss);
+            System.out.println("处理器2执行插入的代码2");
+            return result;
+        };
+        //生成代理对象
+        //(类加载器，接口，处理器)
+        //生成的代理对象拥有这个接口的方法 但是执行这个方法的时候实际上执行处理器的invoke
+        //因此 通过不同的处理器 可以获得不同的代理对象
+        Animal ProxyBird = (Animal) Proxy.newProxyInstance(demo.class.getClassLoader(), bird.getClass().getInterfaces(), handler);
+        ProxyBird.eat();
+        ProxyBird = (Animal) Proxy.newProxyInstance(demo.class.getClassLoader(), bird.getClass().getInterfaces(), handler2);
+        ProxyBird.eat();
+    }
+
+    public interface Animal {
+        void eat();
+    }
+}
