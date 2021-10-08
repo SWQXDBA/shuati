5c8eed73bd8672a487f708be00cdca9daea6bcf7
SWQXDBA <983110853@qq.com>
2021-10-09 02:03:00 +0800

    12

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
 public class Main {
 
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            int k;
+            for (k = 1; k < i; k++) {
+                System.out.print("  ");
+            }
+            for (int j = k; j <= 9; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
 }
