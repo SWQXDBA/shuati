2cc51f5cccada73ad3c6bc9a5944bac66f26b218
SWQXDBA <983110853@qq.com>
2021-10-10 18:02:18 +0800

    12

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,14 @@
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class Main {
 
-    public static void main(String[] args) {
-        ReentrantLock lock = new ReentrantLock();
-        Semaphore semaphoreA = new Semaphore(1);
-        Semaphore semaphoreB = new Semaphore(0);
-        Semaphore semaphoreC = new Semaphore(0);
-        Thread threadA = new Thread(() -> {
-            try {
-                for (int j = 0; j < 10; j++) {
-                    semaphoreA.acquire();
-                    System.out.print("A ");
-                    semaphoreB.release();
-                }
 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        Thread threadB = new Thread(() -> {
-            try {
-                for (int j = 0; j < 10; j++) {
-                    semaphoreB.acquire();
-                    System.out.print("B ");
-                    semaphoreC.release();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        Thread threadC = new Thread(() -> {
-            try {
-                for (int j = 0; j < 10; j++) {
-                    semaphoreC.acquire();
-                    System.out.println("C");
-                    semaphoreA.release();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        threadA.start();
-        threadB.start();
-        threadC.start();
+    static int a;
+
+    static {
+        a = 5;
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
 
     }
 }
