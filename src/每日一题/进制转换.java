e27f128b2fe90b34c4f835b97f7f36dd0d8b669f
SWQXDBA <983110853@qq.com>
2021-10-13 18:03:03 +0800

    12

--- src/每日一题/进制转换.java
+++ src/每日一题/进制转换.java
@@ -7,6 +7,33 @@ public class 进制转换 {
         Scanner scanner = new Scanner(System.in);
         int M = scanner.nextInt();
         int N = scanner.nextInt();
-        System.out.println(Integer.toString(M, N));
+        System.out.println(change(M, N));
+        // System.out.println(Integer.toString(M, N));
     }
+
+    public static String change(int x, int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean flag = false;
+        if (x < 0) {
+            x *= -1;
+            flag = true;
+        }
+        while (x != 0) {
+            int cnt = x % n;
+            if (cnt < 9) {
+                stringBuilder.append(cnt);
+            } else {
+                stringBuilder.append((char) ('A' + cnt - 10));
+            }
+            x /= n;
+
+        }
+        if (flag) {
+            stringBuilder.append("-");
+        }
+        stringBuilder.reverse();
+
+        return stringBuilder.toString();
+    }
+
 }
