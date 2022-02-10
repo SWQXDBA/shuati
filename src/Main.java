3dcd9b0f683933a3341993d82f42ddeb7e9ed923
SWQXDBA <983110853@qq.com>
2022-02-10 13:43:13 +0800

    12

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,23 @@
-import MyTools.工具类.Sleeper;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.stream.Collectors;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
-
-
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list = list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-        int cnt = 20;
-        BlockingQueue<Integer> queue = new LinkedBlockingQueue<>(cnt);
-        Thread product = new Thread(() -> {
-            while (true) {
-                try {
-                    queue.put(1);
-                    System.out.println("生产了1 剩余" + queue.remainingCapacity());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Sleeper.sleep(200);
-            }
-        });
-        Thread consumer = new Thread(() -> {
-            try {
-                while (true) {
-                    queue.take();
-                    System.out.println("消费了1 剩余" + queue.remainingCapacity());
-                    Sleeper.sleep(200);
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        product.start();
-        consumer.start();
+        Main main = new Main();
+        System.out.println(main);
+        main.test();
 
-
+        try {
+            CompletableFuture.runAsync(null).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
-
+    public void test() {
+        System.out.println(Main.this);
+    }
 }
 
