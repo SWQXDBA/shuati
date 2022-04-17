b607d1724d0519c566064c7994545b1dd7014a3b
SWQXDBA <983110853@qq.com>
2022-04-18 01:07:33 +0800

    123

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,38 @@
+import java.util.concurrent.*;
+
 /**
  * @author SWQXDBA
  */
 public class Main {
 
     public static void main(String[] args) {
-        Father father = new Father();
 
+        ThreadFactory threadFactory = new ThreadFactory() {
+            int num = 0;
 
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r, "" + (num++));
+            }
+        };
+        ThreadPoolExecutor executorService = new ThreadPoolExecutor(10, 15, 100, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(1), threadFactory);
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        final Runnable submit = (Runnable) executorService.submit(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                return null;
+            }
+        });
+        //  System.out.println("111");
+        System.out.println(submit);
     }
 
-    static class Father {
-        String name = getName(getName(null));
 
-
-        public Father() {
-            System.out.println("const");
-        }
-
-        private String getName(Object o) {
-            System.out.println("getName" + name);
-            return "555";
-        }
-
-    }
 }
 
