3ca215276d2fe50a616bb1d54cba5c2c55d57282
SWQXDBA <983110853@qq.com>
2022-04-24 03:51:46 +0800

    123

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.math.BigInteger;
+import java.util.Random;
 
 /**
  * @author SWQXDBA
@@ -9,35 +7,13 @@ import java.util.concurrent.Executors;
 public class Main {
 
     public static void main(String[] args) {
-        final ExecutorService service = Executors.newFixedThreadPool(2);
-        long start = System.currentTimeMillis();
-
-        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep(2000L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return "1";
-        }, service);
-        final CompletableFuture<String> objectCompletableFuture = CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep(2000L);
-                System.out.println(System.currentTimeMillis() - start);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return "ok";
-        });
-        final CompletableFuture<String> stringCompletableFuture = future.thenCombine(objectCompletableFuture, (f1, f2) -> {
-            return f1 + f2;
-        });
-
-        try {
-            System.out.println(stringCompletableFuture.get());
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
+        Random random = new Random();
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < 200; i++) {
+            s.append(random.nextInt(9));
         }
+        BigInteger bigInteger = new BigInteger(s.toString());
+        System.out.println(bigInteger.subtract(BigInteger.ONE));
 
     }
 
