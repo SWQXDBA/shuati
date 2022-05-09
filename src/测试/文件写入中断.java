0139844909f8ff0faea939cb0532f1e8212572ea
SWQXDBA <983110853@qq.com>
2022-05-09 18:08:51 +0800

    123

--- src/测试/文件写入中断.java
+++ src/测试/文件写入中断.java
@@ -1,10 +1,31 @@
 package 测试;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
 public class 文件写入中断 {
     public static void main(String[] args) {
+        new Simple<String>() {
+        }.get();
+        new Simple<String>().get();//ClassCastException
+    }
 
-        for (int i = 0; i < 100; i++) {
+    static class Simple<T> {
 
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return super.equals(obj);
+        }
+
+        public void get() {
+            final Type actualTypeArgument = ((ParameterizedType) getClass()
+                    .getGenericSuperclass()).getActualTypeArguments()[0];
+            System.out.println(actualTypeArgument);
         }
     }
 }
