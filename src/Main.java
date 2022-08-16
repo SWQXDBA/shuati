dc735b11a1bc841b414837417bd006a5b8dc9727
SWQXDBA <983110853@qq.com>
2022-08-17 03:39:31 +0800

    123

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
-import java.util.List;
-
-
 /**
  * @author SWQXDBA
  */
 
 public class Main {
-    public static void main(String[] args) {
+    static Thread test() {
+        Execut execut1 = new Execut();
+        final Execut execut = new Execut();
+        return new Thread(execut::fun);
+    }
 
-        List.of(1, 2, 3).parallelStream().forEach(integer -> System.out.println(Thread.currentThread()));
+    public static void main(String[] args) {
+        final Thread test = test();
+        System.gc();
+        test.start();
+    }
+
+    static class Execut {
+        public void fun() {
+            System.out.println(this.hashCode());
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            System.out.println("finalize");
+        }
     }
 
 }
